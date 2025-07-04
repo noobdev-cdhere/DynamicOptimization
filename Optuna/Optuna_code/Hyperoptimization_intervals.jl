b9bb0371a994d3ca65fdbaaeb2d43cc2fe8c0067
noobdev-cdhere <112448314+noobdev-cdhere@users.noreply.github.com>
2025-07-04 21:09:09 +0100

    Added CSV minium_run support, MOEAD fixes, parallelization and case study
    Support for reading the minimum runs for each algorithm from the CSV at the position corresponding to 10% of error for a 95% confidence level
    
    Each algorithms runs for the number of runs red form the CSV
    
    Pycall was linked to a new python environment where the latest version of optuna, as well as its dependencies, were installed
    
    Optuna's code is now partially parallelized with the use of the Distributed library
    
    The hyperparameter "npartitions" from the MOEAD algorithm is now being taken into consideration for HPO
    
    New case study added: parametric_truss_example.jl

--- Optuna/Optuna_code/Hyperoptimization_intervals.jl
+++ Optuna/Optuna_code/Hyperoptimization_intervals.jl
@@ -5,14 +5,14 @@
 
 
     NSGA2_searchspace = OrderedDict(
-        "N" => [5, 500, 100],        # Changed lower from 1 to 10
-        "η_cr" => [1, 100, 100],       # Changed lower from 0 to 1
-        "p_cr" => [0.1, 1, 100],       # Changed lower from 0 to 0.5
-        "η_m" => [1, 100, 100]         # Changed lower from 0 to 1
+        "N" => [5, 500],        # Changed lower from 1 to 10
+        "η_cr" => [1, 100],       # Changed lower from 0 to 1
+        "p_cr" => [0.1, 1],       # Changed lower from 0 to 0.5
+        "η_m" => [1, 100]         # Changed lower from 0 to 1
     )
 
     NSGA3_searchspace = OrderedDict(
-        "N" => [1, 500, 100],
+        "N" => [5, 500, 100],
         "η_cr" => [1, 100, 100],
         "p_cr" => [0, 1, 100],
         "η_m" => [0, 50, 100], 
@@ -24,43 +24,43 @@
     );
 
     SMS_EMOA_searchspace = OrderedDict(
-        "N" => [1, 500, 100],
-        "η_cr" => [0, 100, 100],
-        "p_cr" => [0, 1, 100],
-        "η_m" => [0,100,100],
+        "N" => [5, 500],
+        "η_cr" => [0, 100],
+        "p_cr" => [0, 1],
+        "η_m" => [0,100],
         #p_m => 1.0 / D,
-        "n_samples" => [0, 1000000, 100],
+        "n_samples" => [0, 1000000],
         #information => Information(),
         #options => Options(),
     );
 
     SPEA2_searchspace = OrderedDict(
-    "N" => [1, 500, 100],
-    "η_cr" => [0, 100, 100],
-    "p_cr" => [0, 1, 100],
-    "η_m" => [0,100,100]
+    "N" => [5, 500],
+    "η_cr" => [0, 100],
+    "p_cr" => [0, 1],
+    "η_m" => [0,100]
     #p_m = 1.0 / D,
     #information = Information(),
     #options = Options(),
     );
 
-    MOEAD_DE_searchspace = OrderedDict(
-        "F" => [0.1, 1.0, 0.1],
-        "CR" => [0.1, 1.0, 0.1],
+    MOEAD_DE_searchspace = OrderedDict( 
+        "npartitions" => [5, 30],
+        "F" => [0.1, 1.0],
+        "CR" => [0.1, 1.0],
         #"λ" = Array{Vector{Float64}}[], # ref. points
-        "η" => [5, 50, 5],
+        "η" => [5, 50],
         #"p_m" => [0.0, 1.0, 0.1],
         #"T" = round(Int, 0.2*length(weights)),
-        "δ" =>[0.1, 1.0, 0.1], 
+        "δ" =>[0.1, 1.0], 
         #"n_r" = round(Int, 0.02*length(weights)),
         #"z" => zeros(0),
         #"B" = Array{Int}[],
-        "s1" => [0.001, 0.1, 0.01],
-        "s2" => [1.0, 50.0, 5.0] 
+        "s1" => [0.001, 0.1],
+        "s2" => [1.0, 50.0] 
         #information = Information(),
         #options = Options())
     );
-
 
 
     #NelderMead_searchspace  = OrderedDict(;
---
+++ Study_Frame3dd/parametric_truss_example.jl
@@ -0,0 +1,267 @@
+#using KhepriAutoCAD
+using Pkg
+Pkg.add(url="https://github.com/aptmcl/KhepriFrame3DD.jl")
+using KhepriFrame3DD
+
+# Truss Geometry ---------------------------------------------------------------
+my_free_truss_node_family = truss_node_family_element(default_truss_node_family(), support=false)
+free_node(pt) = truss_node(pt, family=my_free_truss_node_family)
+fixed_node(pt) = truss_node(pt, family=fixed_truss_node_family)
+
+space_frame(ptss) =
+    let ais = ptss[1],
+        bis = ptss[2],
+        cis = ptss[3]
+
+        fixed_node(ais[1])
+        free_node.(ais[2:end-1])
+        fixed_node(ais[end])
+        free_node.(bis)
+        truss_bar.(ais, cis)
+        truss_bar.(bis, ais[1:end-1])
+        truss_bar.(bis, cis[1:end-1])
+        truss_bar.(bis, ais[2:end])
+        truss_bar.(bis, cis[2:end])
+        truss_bar.(ais[2:end], ais[1:end-1])
+        truss_bar.(bis[2:end], bis[1:end-1])
+        if ptss[4:end] == []
+            fixed_node(cis[1])
+            free_node.(cis[2:end-1])
+            fixed_node(cis[end])
+            truss_bar.(cis[2:end], cis[1:end-1])
+        else
+            truss_bar.(bis, ptss[4])
+            space_frame(ptss[3:end])
+        end
+    end
+
+
+
+parametric_truss(x11, y11, z11, x12, y12, z12, x13, y13, z13, x21, y21, z21, x22, y22, z22, x31, y31, z31, x32, y32, z32, x33, y33, z33, x41, y41, z41, x42, y42, z42, x51, y51, z51, x52, y52, z52, x53, y53, z53) =
+    let p11 = xyz(x11, y11, z11),
+        p12 = xyz(x12, y12, z12),
+        p13 = xyz(x13, y13, z13),
+        p21 = xyz(x21, y21, z21),
+        p22 = xyz(x22, y22, z22),
+        p31 = xyz(x31, y31, z31),
+        p32 = xyz(x32, y32, z32),
+        p33 = xyz(x33, y33, z33),
+        p41 = xyz(x41, y41, z41),
+        p42 = xyz(x42, y42, z42),
+        p51 = xyz(x51, y51, z51),
+        p52 = xyz(x52, y52, z52),
+        p53 = xyz(x53, y53, z53)
+
+        space_frame([[p11, p12, p13],
+            [p21, p22],
+            [p31, p32, p33],
+            [p41, p42],
+            [p51, p52, p53]])
+    end
+
+#delete_all_shapes()
+#parametric_truss(0, 0, 0, 1, 0, 0, 2, 0, 0, 0.5, 0.5, 1, 1.5, 0.5, 1, 0, 1, 0, 1, 1, 0, 2, 1, 0, 0.5, 1.5, 1, 1.5, 1.5, 1, 0, 2, 0, 1, 2, 0, 2, 2, 0)
+
+fixed_parametric_truss(
+    x12, y12, z12,
+    x21, y21, z21,
+    x22, y22, z22,
+    x32, y32, z32,
+    x41, y41, z41,
+    x42, y42, z42,
+    x52, y52, z52) =
+    begin
+        delete_all_shapes()
+        parametric_truss(
+            0, 0, 0, x12, y12, z12, 20, 0, 0,
+            x21, y21, z21, x22, y22, z22,
+            0, 10, 0, x32, y32, z32, 20, 10, 0,
+            x41, y41, z41, x42, y42, z42,
+            0, 20, 0, x52, y52, z52, 20, 20, 0)
+    end
+
+#delete_all_shapes()
+#fixed_parametric_truss(
+#    1.3, 0, 0,
+#    0.5, 0.5, 1, 1.5, 0.5, 1,
+#    1, 1, 0,
+#    0.5, 1.5, 1, 1.5, 1.5, 1,
+#    1, 2, 0)
+
+#=
+random_fixed_parametric_truss(r) =
+    let rnd(v) = random_range(v - r, v + r)*10
+        fixed_parametric_truss(
+            rnd(1), rnd(0), rnd(0),
+            rnd(0.5), rnd(0.5), rnd(1),
+            rnd(1.5), rnd(0.5), rnd(1),
+            rnd(1), rnd(1), rnd(0),
+            rnd(0.5), rnd(1.5), rnd(1),
+            rnd(1.5), rnd(1.5), rnd(1),
+            rnd(1), rnd(2), rnd(0))
+    end
+=#
+
+#delete_all_shapes()
+#random_fixed_parametric_truss(0.4)
+
+# Truss Analysis and Optimization ----------------------------------------------
+using Metaheuristics
+
+## Helper Functions
+int2float(x, min, step=0.01) =
+    min + step * x
+
+bounds_coordinates(v, r=0.3) = (v - r, v + r) .* 10
+
+## Materials Young's Modulus and Cost
+materials_e = [
+    1.6409e11, # Cast Iron ASTM A536
+    1.86e11,   # Steel, stainless AISI 302
+    2.e11,     # Carbon Steel, Structural ASTM A516
+    2.0684e11, # Alloy Steel, ASTM A242
+    2.047e11,  # Alloy Steel, AISI 4140
+    1.93e11,   # Stainless Steel AISI 201
+]
+
+materials_cost = [
+    460.0,     # Cast Iron ASTM A536
+    1480.0,    # Steel, stainless AISI 302
+    860.0,     # Carbon Steel, Structural ASTM A516
+    950.0,     # Alloy Steel, ASTM A242
+    2750.0,    # Alloy Steel, AISI 4140
+    1825.0,    # Stainless Steel AISI 201
+]
+
+## Objectives
+#=
+The objectives for the optimization are:
+  (1) minimizing the maximum displacement,
+  (2) minimizing the material cost of the truss structure.
+=#
+n_objs = 2
+
+cost(truss_volume, material) =
+    truss_volume * materials_cost[Int(material)]
+
+objectives(
+    material, bar_radius,
+    x12, y12, z12,
+    x21, y21, z21,
+    x22, y22, z22,
+    x32, y32, z32,
+    x41, y41, z41,
+    x42, y42, z42,
+    x52, y52, z52) =
+    let b_radius = int2float(bar_radius, 0.035, 0.005),
+        load = vz(-3500.0) * 20 * 20 # total load applied to the truss
+        set_backend_family(
+            default_truss_bar_family(),
+            frame3dd,
+            frame3dd_truss_bar_family(
+                E=materials_e[Int(material)], # (Young's modulus)
+                #G=8.1e10,                    # (Kirchoff's or Shear modulus)
+                G=7.95e10,                    # (Kirchoff's or Shear modulus)
+                p=0.0,                        # Roll angle
+                d=7850.0))                    # Density
+                #d=77010.0))                   # Density
+        with_truss_node_family(radius=b_radius * 2.4) do
+            with_truss_bar_family(radius=b_radius, inner_radius=b_radius - 0.02) do
+                fixed_parametric_truss(x12, y12, z12, x21, y21, z21, x22, y22, z22, x32, y32, z32, x41, y41, z41, x42, y42, z42, x52, y52, z52)
+                free_ns = length(filter(!KhepriBase.truss_node_is_supported, frame3dd.truss_nodes))
+                truss_volume = truss_bars_volume()
+                analysis = truss_analysis(load / free_ns)
+                max_disp = max_displacement(analysis)
+                # show_truss_deformation(analysis, autocad, factor=100) # to visualize in AutoCAD
+                [max_disp, cost(truss_volume, material)]
+            end
+        end
+    end
+#=
+problem(x) =
+    (objectives(
+        x[1], x[2], x[3], x[4], x[5],
+        x[6], x[7], x[8], x[9], x[10],
+        x[11], x[12], x[13], x[14], x[15],
+        x[16], x[17], x[18], x[19], x[20],
+        x[21], x[22], x[23]),
+    [0.0], [0.0])
+=#
+
+problem(x) =
+    (objectives(
+            x[:integer][1], x[:integer][2],
+            x[:continuous][1], x[:continuous][2], x[:continuous][3],
+            x[:continuous][4], x[:continuous][5], x[:continuous][6],
+            x[:continuous][7], x[:continuous][8], x[:continuous][9],
+            x[:continuous][10], x[:continuous][11], x[:continuous][12], 
+            x[:continuous][13], x[:continuous][14], x[:continuous][15],
+            x[:continuous][16], x[:continuous][17], x[:continuous][18],
+            x[:continuous][19], x[:continuous][20], x[:continuous][21]),
+        [0.0], [0.0])
+
+## Variables
+n_vars = 23
+
+material_idx = 1:6
+bar_radius = 0:8 # min=0.035, max=0.075, step=0.005
+
+r = 0.4
+x12 = bounds_coordinates(1, r)
+y12 = bounds_coordinates(0, r)
+z12 = bounds_coordinates(0, r)
+x21 = bounds_coordinates(0.5, r)
+y21 = bounds_coordinates(0.5, r)
+z21 = bounds_coordinates(1, r)
+x22 = bounds_coordinates(1.5, r)
+y22 = bounds_coordinates(0.5, r)
+z22 = bounds_coordinates(1, r)
+x32 = bounds_coordinates(1, r)
+y32 = bounds_coordinates(1, r)
+z32 = bounds_coordinates(0, r)
+x41 = bounds_coordinates(0.5, r)
+y41 = bounds_coordinates(1.5, r)
+z41 = bounds_coordinates(1, r)
+x42 = bounds_coordinates(1.5, r)
+y42 = bounds_coordinates(1.5, r)
+z42 = bounds_coordinates(1, r)
+x52 = bounds_coordinates(1, r)
+y52 = bounds_coordinates(2, r)
+z52 = bounds_coordinates(0, r)
+
+points = [x12, y12, z12,
+          x21, y21, z21,
+          x22, y22, z22,
+          x32, y32, z32,
+          x41, y41, z41,
+          x42, y42, z42,
+          x52, y52, z52]
+
+points_lb = [p[1] for p in points]
+points_ub = [p[end] for p in points]
+
+integer_space = BoxConstrainedSpace([material_idx[1], bar_radius[1]], [material_idx[end], bar_radius[end]])
+continuous_space = BoxConstrainedSpace(points_lb, points_ub)
+mixed_space = MixedSpace(:integer => integer_space, :continuous => continuous_space)
+
+#=
+vars_bounds =
+    [material_idx[1] bar_radius[1] x12[1] y12[1] z12[1] [
+     x21[1]] y21[1] z21[1] x22[1] y22[1] z22[1] [
+     x32[1]] y32[1] z32[1] x41[1] y41[1] z41[1] [
+     x42[1]] y42[1] z42[1] x52[1] y52[1] z52[1];
+     material_idx[end] bar_radius[end] x12[end] y12[end] z12[end] [
+     x21[end]] y21[end] z21[end] x22[end] y22[end] z22[end] [
+     x32[end]] y32[end] z32[end] x41[end] y41[end] z41[end] [
+     x42[end]] y42[end] z42[end] x52[end] y52[end] z52[end]]
+=#
+
+## Test Optimization
+pop_size = 15
+n_iterations = 20
+max_evals = pop_size * n_iterations
+
+options = Options(iterations=n_iterations, f_calls_limit=3*max_evals)
+nsga2 = MixedInteger(NSGA2(N=pop_size, options=options))
+
+results = optimize(problem, mixed_space, nsga2)
